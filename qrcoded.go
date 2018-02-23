1eef46ca124f806ef2b281af523f03e4e786dd9d
Mihalis <mihalisfthenos@icloud.com>
2018-02-23 07:47:49 -0500

    add commenting

--- qrcoded.go
+++ qrcoded.go
@@ -10,20 +10,25 @@ import (
 
 func main() {
 	log.Println("Hello QR Code")
-
+	// Create png file
 	file, err := os.Create("qrcode.png")
+	// Error handling
 	if err != nil {
 		log.Fatal(err) // automatically calls os.Exit(1)
 	}
+	// Close file when func is finished running
 	defer file.Close()
-
+	// Generate the QR code and catch any error
 	err = GenerateQRCode(file, "555-3556")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// GenerateQRCode Generates a QR Code from a string
 func GenerateQRCode(w io.Writer, code string) error {
+	// NRGBA is an in-memory image whose At method returns color.NRGBA values.
 	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
+	// Save the png to file and return any error
 	return png.Encode(w, img)
 }
